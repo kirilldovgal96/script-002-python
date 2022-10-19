3f8af0d859ea6abd7d5394efb3527852cb58f141
kirill <kirill.dovgal96@mail.ru>
2022-10-19 19:59:12 +0300

    answ_odd_even

---
+++ 01_basics/odd_even/answ_odd_even.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+chislo = int(input('Введите целое число: '))
+if (chislo% 2) == 0:
+    print(f'Введённое число - чётное')
+else:
+    print(f'Введённое число нечётное')
+
+#print(f'По курсу {kurs} на {kollich} рублей вы купите {summa:.2f} евро.')
